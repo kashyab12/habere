a01d9bf9a0dc4de39e1749249d0da6ae95ae2ebd
kashyaba <iamkashyab@gmail.com>
2024-03-21 23:52:34 -0700

    declare chi routers

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+	"github.com/kashyab12/habere/handlers"
+	"os"
+)
 
 func main() {
-	fmt.Println("habere: stub, work the fe first")
+	err := godotenv.Load()
+	if err != nil {
+		return
+	}
+	appRouter := chi.NewRouter()
+	apiRouter := chi.NewRouter()
+	apiConfig := handlers.ApiConfig{ClientID: os.Getenv("CLIENT_ID")}
+	apiRouter.Get("/ttAuth", apiConfig.GetTTAuthHandler)
+	appRouter.Mount("/api", apiRouter)
 }
