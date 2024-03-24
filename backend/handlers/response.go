035373ed1b636eb1fcdfabc10d47ef0c0c32aef6
kashyaba <iamkashyab@gmail.com>
2024-03-24 16:23:53 -0700

    Get rid of error handler

--- backend/handlers/response.go
+++ backend/handlers/response.go
@@ -1,17 +1,1 @@
 package handlers
-
-import (
-	"errors"
-	"github.com/gofiber/fiber/v3"
-)
-
-func DefaultErrorHandler(c fiber.Ctx, err error) error {
-	code := fiber.StatusInternalServerError
-	var e *fiber.Error
-	if errors.As(err, &e) {
-		code = e.Code
-	}
-	return c.Status(code).JSON(fiber.Map{
-		"error": err.Error(),
-	})
-}
