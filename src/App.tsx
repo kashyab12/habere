0afb91f5d8879e765d49b92dd68b0c588e6ba3c5
kashyaba <iamkashyab@gmail.com>
2024-03-25 02:59:43 -0700

    have a popup window for the auth url

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
         <>
             <h1>habere</h1>
             <div className="card">
-                <LoginWithTickTick />
+                <LoginWithTickTick buttonInfo={"Login with TickTick"}/>
                 <p>
                     Click the above to authenticate with TickTick.
                 </p>
--- src/Login.tsx
+++ src/Login.tsx
@@ -1,40 +1,39 @@
 import './App.css'
 import {constants} from './constants.ts'
+import {useRef} from "react";
 
 function LoginButton({onClickFunc, buttonInfo}) {
+
+
+}
+
+function LoginWithTickTick({buttonInfo}) {
+    const onClickHandler = async () =>  {
+        try {
+            const corsHeader: HeadersInit = new Headers()
+            const fetchOptions: RequestInit = {
+                method: "GET",
+                mode: "cors",
+                credentials: "include"
+            }
+            const getRedirectResponse = await fetch(constants.BACKEND_TT_REDIR_EP, fetchOptions)
+            const jsonBody = await getRedirectResponse.json()
+            if (jsonBody?.url) {
+                const popupWindow = window.open(jsonBody.url, "TickTick Login", "width=600,height=400")
+            } else {
+                console.log("received invalid response, toast with error")
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    }
     return (
         <>
-            <button onClick={redirectOnClickHandler}>
+            <button onClick={onClickHandler}>
                 {buttonInfo}
             </button>
         </>
     )
-}
-
-function LoginWithTickTick() {
-    return (
-        <LoginButton onClick={redirectOnClickHandler} buttonInfo="Login with TickTick"/>
-    )
-}
-
-const redirectOnClickHandler = async () => {
-    try {
-        const corsHeader: HeadersInit = new Headers()
-        const fetchOptions: RequestInit = {
-            method: "GET",
-            mode: "cors",
-            credentials: "include"
-        }
-        const getRedirectResponse = await fetch(constants.BACKEND_TT_REDIR_EP, fetchOptions)
-        const jsonBody = await getRedirectResponse.json()
-        if (jsonBody?.url) {
-            window.open(jsonBody.url, '_blank')
-        } else {
-            console.log("received invalid response, toast with error")
-        }
-    } catch (e) {
-        console.log(e)
-    }
 }
 
 export default LoginWithTickTick
