30a0e673a1cda6d84662d1ad4941eca4bf8d85b1
kashyaba <iamkashyab@gmail.com>
2024-03-27 03:19:11 -0700

    hmm, cors is not playing well with the popup window. May need to look further into postMessage() for cross-origin window comms

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,9 +1,9 @@
 import './App.css'
 import {constants} from './constants.ts'
-import {useRef} from "react";
+// import {useRef} from "react";
 
 
-function LoginWithTickTick({buttonInfo}) {
+function LoginWithTickTick({buttonInfo}: {buttonInfo: string}) {
     const onClickHandler = async () =>  {
         try {
             const fetchOptions: RequestInit = {
@@ -16,6 +16,14 @@ function LoginWithTickTick({buttonInfo}) {
             if (jsonBody?.url) {
                 // TODO: DOM related methods and event handlers, or can some react related methods be useful here?
                 const popupWindow = window.open(jsonBody.url, "TickTick Login", "width=600,height=400")
+                popupWindow?.addEventListener("storage", (sessionStoreEvent) =>  {
+                    console.log("storage event!")
+                    // TODO: how will we handle routing to logout route? Perhaps route after a timeout, not ideal though?
+                    if (sessionStoreEvent.key?.toLowerCase().includes("authentication")) {
+                        popupWindow.close()
+                        // TODO: Route to login route.
+                    }
+                })
             } else {
                 console.log("received invalid response, toast with error")
             }
