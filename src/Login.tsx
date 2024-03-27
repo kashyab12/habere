850ff530b9a5b3005f8b1a86dec3d12abc241d3b
kashyaba <iamkashyab@gmail.com>
2024-03-26 23:36:25 -0700

    learning more react lol

--- src/Login.tsx
+++ src/Login.tsx
@@ -19,6 +19,7 @@ function LoginWithTickTick({buttonInfo}) {
             const getRedirectResponse = await fetch(constants.BACKEND_TT_REDIR_EP, fetchOptions)
             const jsonBody = await getRedirectResponse.json()
             if (jsonBody?.url) {
+                // TODO: DOM related methods and event handlers, or can some react related methods be useful here?
                 const popupWindow = window.open(jsonBody.url, "TickTick Login", "width=600,height=400")
             } else {
                 console.log("received invalid response, toast with error")
