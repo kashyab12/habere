d0c430270bec792759a751bbcc0208d1b5f04be2
kashyaba <iamkashyab@gmail.com>
2024-04-05 22:12:18 -0700

    refactor, let's use next to hopefully speed things up

--- src/constants.ts
+++
@@ -1,8 +0,0 @@
-export const constants = {
-    DEFAULT_REDIRECT: "https://google.com",
-    BACKEND_URL: "http://localhost:8080",
-    BACKEND_TT_REDIR_EP: "http://localhost:8080/ttScopeVerify",
-    VALID_SESSION_ENDPOINT: "http://localhost:8080/isValidTTSession",
-    TT_TODAYS_TASKS: "http://localhost:8080/todaysTasks",
-    HTTP_STATUS_OK: 200
-}
