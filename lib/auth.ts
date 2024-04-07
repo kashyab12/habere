d3e57dbea2570e70eaf07abded5bec55f96fde22
kashyaba <iamkashyab@gmail.com>
2024-04-07 14:19:01 -0700

    define an oslo client instead

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,17 +1,5 @@
-import {type OAuthConfig} from "next-auth/providers/oauth";
-
-export const ttProvider= {
-    id: "ticktick",
-    name: "TickTick",
-    type: "oauth",
-    clientId: process.env.TT_CLIENT_ID,
-    clientSecret: process.env.TT_CLIENT_SECRET,
-    authorization: {
-        url: "https://ticktick.com/oauth/authorize",
-        params: {
-            scope: "tasks:write tasks:read",
-            response_type: "code"
-        }
-    },
-    token: "https://ticktick.com/oauth/token",
-} as OAuthConfig<any>
+import { OAuth2Client } from "oslo/oauth2";
+const authorizeEndpoint = "https://ticktick.com/oauth/authorize"
+const tokenEndpoint = "https://ticktick.com/oauth/token"
+const redirectURI = "http://localhost:3000/api/auth/callback/ticktick"
+export const ttClient = new OAuth2Client(process.env.TT_CLIENT_ID ?? "", authorizeEndpoint, tokenEndpoint, {redirectURI})
