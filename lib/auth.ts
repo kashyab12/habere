59fb316ecdda8b4bb040b82dba6176024ee8977f
kashyaba <iamkashyab@gmail.com>
2024-04-07 14:39:27 -0700

    formatting fix

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,4 +2,4 @@ import { OAuth2Client } from "oslo/oauth2";
 const authorizeEndpoint = "https://ticktick.com/oauth/authorize"
 const tokenEndpoint = "https://ticktick.com/oauth/token"
 const redirectURI = "http://localhost:3000/api/auth/callback/ticktick"
-export const ttClient = new OAuth2Client(process.env.TT_CLIENT_ID ?? "", authorizeEndpoint, tokenEndpoint, {redirectURI})
+export const ttClient = new OAuth2Client(process.env.TT_CLIENT_ID ?? "", authorizeEndpoint, tokenEndpoint, { redirectURI })
