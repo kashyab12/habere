0e8da31e46cef8e4f4b826698cf8823cb589e4a8
kashyaba <iamkashyab@gmail.com>
2024-04-10 03:44:02 -0700

    added table component and get rid of unneeded imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import { Dashboard } from "@/components/ui/login";
 import SignInContent from "@/components/ui/signin-content";
 import TaskCards from "@/components/ui/task-cards";
-import { HabereSessionData, habereSessionOption } from "@/lib/auth";
 import getTodaysTasks from "@/lib/tasks";
-import { getIronSession } from "iron-session";
-import { cookies, headers } from "next/headers";
+import { headers } from "next/headers";
 
 async function Home() {
   const authHeader = headers().get("Authorization")  
