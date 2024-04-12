23649f017177afcdab582bbbc81f0b946ff60fe2
kashyaba <iamkashyab@gmail.com>
2024-04-12 05:18:10 -0700

    perhaps fix the redirect complains from chrome

--- middleware.ts
+++ middleware.ts
@@ -5,22 +5,26 @@ import { HabereSessionData, habereSessionOption } from "@/lib/auth";
 
 export const config = {
     matcher: [
-      /*
-       * Match all request paths except for the ones starting with:
-       * - api (API routes)
-       * - _next/static (static files)
-       * - _next/image (image optimization files)
-       * - favicon.ico (favicon file)
-       */
-      '/((?!api|_next/static|_next/image|favicon.ico).*)',
+        /*
+         * Match all request paths except for the ones starting with:
+         * - api (API routes)
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         */
+        '/((?!api|_next/static|_next/image|favicon.ico).*)',
     ],
 }
 
 export async function middleware(request: NextRequest) {
     const session = await getIronSession<HabereSessionData>(cookies(), habereSessionOption)
-    if (!session.isLoggedIn || !session.accessToken) {
-        // Navigate to the sign in page basically
-        return NextResponse.redirect(new URL('/', request.url))
+    if ((!session.isLoggedIn || !session.accessToken)) {
+        if (request.nextUrl.pathname != "/") {
+            return NextResponse.next()
+        } else {
+            // Navigate to the sign in page basically
+            return NextResponse.redirect(new URL('/', request.url))
+        }
     } else {
         const authHeader = `Bearer ${session.accessToken}`
         const response = NextResponse.next({
