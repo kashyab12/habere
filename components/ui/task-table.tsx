76fa8aadf277117efa3616a75d552aeb61232410
kashyaba <iamkashyab@gmail.com>
2024-04-13 03:50:40 -0700

    draft: new task table component to render the table

---
+++ components/ui/task-table.tsx
@@ -0,0 +1,51 @@
+'use client';
+import { getModelOutput } from "@/app/actions"
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table"
+import { DisplayTask } from "@/lib/model"
+import { Task } from "@/lib/tasks"
+import { useEffect, useState } from "react"
+
+export default function TaskTable({pendingTasks}: {pendingTasks: Task[]}) {
+    const today = new Date()
+    const [modelOutput, setModelOutput] = useState<DisplayTask[]>([])
+    useEffect(() => {
+        const updateModelOutput = async () => {
+            const modelOutput = await getModelOutput(pendingTasks, today)
+            setModelOutput(modelOutput)
+        }
+        updateModelOutput()
+    })
+    return (
+        < Table >
+          <TableCaption>Today&apos;s tasks!</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Priority</TableHead>
+              <TableHead>Title</TableHead>
+              <TableHead>Why</TableHead>
+              <TableHead>Time to finish</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {modelOutput.map((task, index) => {
+              return (
+                <TableRow key={index}>
+                  <TableCell className="font-semibold">{task.priority}</TableCell>
+                  <TableCell>{task.taskTitle}</TableCell>
+                  <TableCell>{task.why}</TableCell>
+                  <TableCell>{`${task.expectedTimeToFinish} minutes`}</TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table >
+    )
+}
