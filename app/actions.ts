c231fdcc0c129a8227d8ee4e1c1c51c7b504169f
kashyaba <iamkashyab@gmail.com>
2024-04-13 04:46:04 -0700

    server action returns the modelOutput

--- app/actions.ts
+++ app/actions.ts
@@ -1,11 +1,16 @@
 'use server'
 
-import { cachedInference } from "@/lib/model";
+import { DisplayTask, cachedInference } from "@/lib/model";
 import { Task, getTodaysTask } from "@/lib/tasks";
 
-export async function getModelOutput(pendingTasks: Task[], today: Date) {
+export async function getModelOutput(pendingTasks: Task[], today: Date): Promise<DisplayTask[]> {
     const todaysTasks = getTodaysTask(pendingTasks, today)
     const inferObj = await cachedInference(todaysTasks)
     const inferResp = JSON.parse(inferObj.choices?.[0]?.message?.content ?? "{}")
-    return inferResp
+    if (!inferResp?.['prioritizedTaskList']) {
+        return []
+    } else {
+        const modelOutput = inferResp['prioritizedTaskList'] as DisplayTask[]
+        return modelOutput
+    }
 }
