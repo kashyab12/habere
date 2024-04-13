2b4ff55472f57524ef8887dbfdae015f2b807bdb
kashyaba <iamkashyab@gmail.com>
2024-04-13 16:55:50 -0700

    assign dep to useEffect to avoid constant re-renders

--- components/ui/task-table.tsx
+++ components/ui/task-table.tsx
@@ -1,51 +1,51 @@
 'use client';
 import { getModelOutput } from "@/app/actions"
 import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-  } from "@/components/ui/table"
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 import { DisplayTask } from "@/lib/model"
 import { Task } from "@/lib/tasks"
 import { useEffect, useState } from "react"
 
-export default function TaskTable({pendingTasks}: {pendingTasks: Task[]}) {
+export default function TaskTable({ pendingTasks }: { pendingTasks: Task[] }) {
+  const [modelOutput, setModelOutput] = useState<DisplayTask[]>([])
+  useEffect(() => {
     const today = new Date()
-    const [modelOutput, setModelOutput] = useState<DisplayTask[]>([])
-    useEffect(() => {
-        const updateModelOutput = async () => {
-            const modelOutput = await getModelOutput(pendingTasks, today)
-            setModelOutput(modelOutput)
-        }
-        updateModelOutput()
-    })
-    return (
-        < Table >
-          <TableCaption>Today&apos;s tasks!</TableCaption>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[100px]">Priority</TableHead>
-              <TableHead>Title</TableHead>
-              <TableHead>Why</TableHead>
-              <TableHead>Time to finish</TableHead>
+    const updateModelOutput = async () => {
+      const modelOutput = await getModelOutput(pendingTasks, today)
+      setModelOutput(modelOutput)
+    }
+    updateModelOutput()
+  }, [pendingTasks])
+  return (
+    < Table >
+      <TableCaption>Today&apos;s tasks!</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Priority</TableHead>
+          <TableHead>Title</TableHead>
+          <TableHead>Why</TableHead>
+          <TableHead>Time to finish</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {modelOutput.map((task, index) => {
+          return (
+            <TableRow key={index}>
+              <TableCell className="font-semibold">{task.priority}</TableCell>
+              <TableCell>{task.taskTitle}</TableCell>
+              <TableCell>{task.why}</TableCell>
+              <TableCell>{`${task.expectedTimeToFinish} minutes`}</TableCell>
             </TableRow>
-          </TableHeader>
-          <TableBody>
-            {modelOutput.map((task, index) => {
-              return (
-                <TableRow key={index}>
-                  <TableCell className="font-semibold">{task.priority}</TableCell>
-                  <TableCell>{task.taskTitle}</TableCell>
-                  <TableCell>{task.why}</TableCell>
-                  <TableCell>{`${task.expectedTimeToFinish} minutes`}</TableCell>
-                </TableRow>
-              )
-            })}
-          </TableBody>
-        </Table >
-    )
+          )
+        })}
+      </TableBody>
+    </Table >
+  )
 }
